d7c0e8addf0cc997dd5a443ee6532340aa836e2f
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-09 00:54:12 -0500

    [unity] tictoc challenge

---
+++ Unity-TicTacToe-challenge/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerColor
+{
+    public Color panelColor;
+    public Color textColor;
+}
+
+[System.Serializable]
+public class Player
+{
+    public Image panel;
+    public Text text;
+}
+
+
+public class GameControllerScript : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+
+    public GameObject gameOverPanel;
+    public Text gamerOverText;
+
+    private int moveCount;
+
+    public GameObject restarButton;
+
+    public Player playerX;
+    public Player playerO;
+    public PlayerColor activePlayerColor;
+    public PlayerColor inactivePlayerColor;
+
+    void Awake()
+    {
+        gameOverPanel.SetActive(false);
+        SetGameControllerReferenceOnButton();
+        playerSide = "X";
+        moveCount = 0;
+        SetPlayerColors(playerX, playerO);
+    }
+
+    void SetGameControllerReferenceOnButton()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpaceScript>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        moveCount++;
+
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if(moveCount >= 9)
+        {
+            SetGameOver("It's a draw!");
+        }
+
+        ChangeSides();
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            SetBoardInteractable(false);
+        }
+
+        SetGameOver(playerSide + " Wins!");
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+
+        if(playerSide == "X")
+        {
+            SetPlayerColors(playerX, playerO);
+        }else
+        {
+            SetPlayerColors(playerO, playerX);
+        }
+    }
+
+    void SetGameOver(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gamerOverText.text = value;
+    }
+
+    public void RestarGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            SetBoardInteractable(true);
+            buttonList[i].text = "";
+        }
+
+        SetPlayerColors(playerX, playerO);
+    }
+
+    void SetBoardInteractable(bool toggle)
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+        }
+    }
+
+    void SetPlayerColors(Player newPlayer, Player oldPlayer)
+    {
+        newPlayer.panel.color = activePlayerColor.panelColor;
+        newPlayer.text.color = activePlayerColor.textColor;
+        oldPlayer.panel.color = inactivePlayerColor.panelColor;
+        oldPlayer.text.color = inactivePlayerColor.textColor;
+    }
+}
